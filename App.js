6423e5163f59b3e1081c2b1c0e5b2b0cf00bf52b
Bindu3656 <bindu.mutyam@sasi.ac.in>
2024-01-31 15:50:56 +0530

    react

---
+++ App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import RegistrationForm from './RegistrationForm';
+
+const App = () => {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default App;
